1bd07f2fc4450e9623a255cc0bc0540b007c8508
Nathan <nathanyong@airmail.cc>
2018-04-14 18:22:43 -0500

    Moved edit maxHP to seperate function

--- csSaveEditor/Editor.cs
+++ csSaveEditor/Editor.cs
@@ -7,16 +7,31 @@ namespace csSaveEditor
 	{
 		public void editHP()
 		{
+			Console.Clear();
 			int hp;
-			int maxHp;
+			Console.WriteLine("HP Editor");
+			Console.WriteLine("---------------------------");
 			Console.WriteLine("Enter the desire HP: ");
 			hp = Convert.ToInt32(Console.ReadLine());
-			Console.WriteLine("Enter the desire max HP: ");
-			maxHp = Convert.ToInt32(Console.ReadLine());
+
 			using (BinaryWriter bw = new BinaryWriter(new FileStream("Profile.dat", FileMode.Open)))
 			{
 				bw.BaseStream.Seek(32, SeekOrigin.Begin);
 				bw.Write(hp);
+				bw.Close();
+			}
+		}
+
+		public void editMaxHp()
+		{
+			Console.Clear();
+			int maxHp;
+			Console.WriteLine("HP Editor");
+			Console.WriteLine("---------------------------");
+			Console.WriteLine("Enter the desire max HP: ");
+			maxHp = Convert.ToInt32(Console.ReadLine());
+			using (BinaryWriter bw = new BinaryWriter(new FileStream("Profile.dat", FileMode.Open)))
+			{
 				bw.BaseStream.Seek(28, SeekOrigin.Begin);
 				bw.Write(maxHp);
 				bw.Close();
@@ -26,14 +41,20 @@ namespace csSaveEditor
 		public void viewData()
 		{
 			Console.Clear();
+			Console.WriteLine("Data");
+			Console.WriteLine("---------------------------");
 			int currentMap;
 			int currentHp;
 			int maxHp;
+			int facing;
 			byte[] values = new byte[21];
 			using (BinaryReader br = new BinaryReader(new FileStream("Profile.dat", FileMode.Open)))
 			{
 				br.BaseStream.Seek(8, SeekOrigin.Begin);
 				currentMap = br.ReadByte();
+
+				br.BaseStream.Seek(24, SeekOrigin.Begin);
+				facing = br.ReadByte();
 
 				br.BaseStream.Seek(28, SeekOrigin.Begin);
 				maxHp = br.ReadByte();
@@ -44,6 +65,14 @@ namespace csSaveEditor
 				Console.WriteLine("Current map: {0}", currentMap);
 				Console.WriteLine("Current HP : {0}", currentHp);
 				Console.WriteLine("Max HP: {0}", maxHp);
+				if (facing == 0)
+				{
+					Console.WriteLine("Facing: left");
+				}
+				else if (facing == 2)
+				{
+					Console.WriteLine("Facing: right");
+				}
 
 				Console.WriteLine("<press enter>");
 				Console.ReadLine();
@@ -54,7 +83,24 @@ namespace csSaveEditor
 
 		public void editAmmo()
 		{
+			int ammo;
+			int maxAmmo;
+			int currentWeapon;
+			Console.Clear();
+			Console.WriteLine("Enter desire ammo: ");
+			ammo =  Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Enter desired max ammo: ");
+			maxAmmo = Convert.ToInt32(Console.ReadLine());
+			using (BinaryReader br = new BinaryReader(new FileStream("Profile.dat", FileMode.Open)))
+			{
+				br.BaseStream.Seek(36, SeekOrigin.Begin);
+				currentWeapon = br.ReadByte();
+			}
+			using (BinaryWriter bw = new BinaryWriter(new FileStream("Profile.dat", FileMode.Open)))
+			{
+				bw.BaseStream.Seek(currentWeapon, SeekOrigin.Begin);
 
+			}
 		}
 	}
 }
--- csSaveEditor/Program.cs
+++ csSaveEditor/Program.cs
@@ -15,12 +15,15 @@ namespace csSaveEditor
 		{
 			Editor editor = new Editor();
 			string choice;
+			Console.BackgroundColor = ConsoleColor.Blue;
 			while (true)
 			{
 				Console.Clear();
 				Console.WriteLine("CaveStory Save Editor");
+				Console.WriteLine("---------------------------");
 				Console.WriteLine("1) View Data");
 				Console.WriteLine("2) Edit HP");
+				Console.WriteLine("3) Edit Max HP");
 				choice = Console.ReadLine();
 				if (choice == "1")
 				{
@@ -29,6 +32,10 @@ namespace csSaveEditor
 				else if (choice == "2")
 				{
 					editor.editHP();
+				}
+				else if (choice == "3")
+				{
+					editor.editMaxHp();
 				}
 				else
 				{
